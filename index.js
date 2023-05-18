b8c814c925629226450529370e0e59efaa2de9f9
spt4m1m <white.hat.tamim@gmail.com>
2023-05-18 11:50:55 +0600

    connect with database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const path = require('path');
 const cors = require('cors');
+require('dotenv').config()
 const colors = require('colors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +12,43 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.negee10.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(`animals-toyhub DATABASE Connected`.cyan);
+
+
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.post('/alltoys', (req, res) => {
+    const toyData = req.body;
+    console.log(toyData)
 })
 
 app.listen(port, () => {
